f730b3414a2b23f31c26f39ed537e065d32bab99
Dusan Litvik <litvik@metait.cz>
2019-04-30 18:27:42 +0200

    - another shit happened. Maybe working?

---
+++ src/main/java/cz/fi/muni/pa036/cachingapp/dto/BranchDTO.java
@@ -0,0 +1,58 @@
+package cz.fi.muni.pa036.cachingapp.dto;
+
+public class BranchDTO {
+    private Long id;
+    private String name;
+    private String street;
+    private Integer streetNumber;
+    private String city;
+    private String state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
